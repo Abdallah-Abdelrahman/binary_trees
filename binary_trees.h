eaf71951efb555d91197d3eed77786d06242895e
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-01-29 21:12:07 +0300

    Create new node and insert node on the left

--- binary_trees.h
+++ binary_trees.h
@@ -3,6 +3,7 @@
 
 /*----------- HEADERS ------------*/
 #include <stdlib.h>
+#include <stdio.h>
 
 /*----------- MACROS ------------*/
 
@@ -49,5 +50,8 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
+/*----------- HELPERS ------------*/
+void binary_tree_print(const binary_tree_t *tree);
 
 #endif /* HEADER */
