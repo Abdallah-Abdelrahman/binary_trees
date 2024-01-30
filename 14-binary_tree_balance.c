a9ccbec5f5d59921b3a981604b3775ef545dd603
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-01-30 10:16:07 +0300

    Measure factor 000

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -14,9 +14,30 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left)
-		left = binary_tree_balance(tree->left) + 1;
+		left = binary_tree_height(tree->left) + 1;
 	if (tree->right)
-		right = binary_tree_balance(tree->right) + 1;
+		right = binary_tree_height(tree->right) + 1;
 
 	return (left - right);
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree
+ *
+ * Return: height of the tree, 0 if NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left = 0, right = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		left = binary_tree_height(tree->left) + 1;
+	if (tree->right)
+		right = binary_tree_height(tree->right) + 1;
+
+	return ((right > left) ? right : left);
 }
