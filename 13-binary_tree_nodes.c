6505961c4e24593b6cd0c590d3e59ca281044f07
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-01-30 09:11:56 +0300

    Fix count nodes and tree balance

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -17,8 +17,8 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (!tree->left && !tree->right)
 		return (0);
 
-	left = binary_tree_height(tree->left) + 1;
-	right = binary_tree_height(tree->right) + 1;
+	left = binary_tree_nodes(tree->left) + 1;
+	right = binary_tree_nodes(tree->right) + 1;
 
 	return (left + right - 1);
 }
--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -14,9 +14,9 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left)
-		left = binary_tree_height(tree->left) + 1;
+		left = binary_tree_balance(tree->left) + 1;
 	if (tree->right)
-		right = binary_tree_height(tree->right) + 1;
+		right = binary_tree_balance(tree->right) + 1;
 
 	return (left - right);
 }
