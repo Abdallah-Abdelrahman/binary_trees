a7e49172e8732e54e5a7fb6c008b20b5c1173763
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-01-31 17:44:12 +0300

    Update RR 002

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -10,14 +10,16 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
 	binary_tree_t *root, *pivot;
 
-	if (!tree)
+	if (!tree || !tree->right)
 		return (tree);
 
 	root = tree;
-	pivot = root->right ? root->right : root->left;
+	pivot = root->right;
 	pivot->parent = root->parent;
 	root->parent = pivot;
 	root->right = pivot->left;
+	if (root->right)
+		root->right->parent = root;
 	pivot->left = root;
 
 	return (pivot);
--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -10,14 +10,16 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
 	binary_tree_t *root, *pivot;
 
-	if (!tree || (!tree->left && !tree->right))
+	if (!tree || (!tree->left))
 		return (tree);
 
 	root = tree;
-	pivot = root->left ? root->left : root->right;
+	pivot = root->left;
 	pivot->parent = root->parent;
 	root->parent = pivot;
 	root->left = pivot->right;
+	if (root->left)
+		root->left->parent = root;
 	pivot->right = root;
 
 	return (pivot);
