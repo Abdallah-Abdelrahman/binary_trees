e7ccabc6fcccef9fbae8cf3ff377a8db76aa6c0a
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-01-31 21:34:21 +0300

    Update is_complete 001

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,44 +1,55 @@
 #include "binary_trees.h"
-/**
- * isCompleted - checks if a tree is completed
- * @root: racine del'arbre binaire
- * @ind: index
- * @cal: taille of the tree
- *
- * Return: index if completed , 0 otherwise
-*/
-int isCompleted(const binary_tree_t *root, unsigned long cal,
-		unsigned long int ind)
-{
-	if (!root)
-		return (1);
-	if (ind > cal)
-		return (0);
-	return (isCompleted(root->left, 2 * ind + 1, cal)
-			&& isCompleted(root->right, 2 * ind + 2, cal));
-}
+
 /**
  * binary_tree_is_complete - checks if a binary tree is complete
- * @tree: a pointer to the root node of the tree to check
- * Return: 1 if the tree is complete, 0 otherwise
-*/
+ * @tree: pointer to the root node of the tree to check
+ *
+ * Return: 1 if it's complete, 0 if tree is NULL,
+ * or it's not complete
+ */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-	unsigned long cal = binary_tree_size((const binary_tree_t *)tree);
+	queue_t q = {0, 0, NULL};
+	const binary_tree_t *tmp;
+	int is_not_full = 0;
 
 	if (!tree)
 		return (0);
-	return (isCompleted(tree, cal, 0));
-}
-/**
- * binary_tree_size - measures the size of a binary tree
- * @tree: pointer to the root node of the tree to measure the size
- *
- * Return: size of tree, or 0 if tree is NULL
- */
-size_t binary_tree_size(const binary_tree_t *tree)
-{
-	if (!tree)
-		return (0);
-	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+
+	q.queue = calloc(sizeof(binary_tree_t), QUEUE_SIZE);
+	q.queue[q.rear++] = (binary_tree_t *)tree;
+	while (q.rear != q.front)
+	{
+		/* dequeue */
+		tmp = q.queue[q.front++];
+		if (tmp->left)
+		{
+			if (is_not_full)
+			{
+				free(q.queue);
+				return (0);
+			}
+
+			/* enqueue */
+			q.queue[q.rear++] = tmp->left;
+		}
+		else
+			is_not_full = 1;
+		if (tmp->right)
+		{
+			if (is_not_full)
+			{
+				free(q.queue);
+				return (0);
+			}
+
+			/* enqueue */
+			q.queue[q.rear++] = tmp->right;
+		}
+		else
+			is_not_full = 1;
+	}
+
+	free(q.queue);
+	return (1);
 }
