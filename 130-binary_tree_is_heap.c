fdd4ba1183e05f29ad8879eccd26705fa01437bb
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-02-04 19:48:18 +0300

    Check max heap 000

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -1,5 +1,5 @@
 #include "binary_trees.h"
-void hack_betty(void);
+int is_heap(const binary_tree_t *tree);
 
 /**
  * binary_tree_is_heap - checks if a binary tree is a valid Max Binary Heap
@@ -9,6 +9,40 @@ void hack_betty(void);
  * 0 otherwise
  */
 int binary_tree_is_heap(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (!binary_tree_is_complete(tree))
+		return (0);
+
+	return (is_heap(tree));
+}
+
+/**
+ * is_heap - helper function to check recursively
+ * @tree: pointer to root node
+ *
+ * Return: 1 if max heap, 0 otherwise
+ */
+int is_heap(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (1);
+	if (tree->left && tree->n < tree->left->n)
+		return (0);
+	if (tree->right && tree->n < tree->right->n)
+		return (0);
+	return (is_heap(tree->left) && is_heap(tree->right));
+}
+
+/**
+ * binary_tree_is_complete - checks if a binary tree is complete
+ * @tree: pointer to the root node of the tree to check
+ *
+ * Return: 1 if it's complete, 0 if tree is NULL,
+ * or it's not complete
+ */
+int binary_tree_is_complete(const binary_tree_t *tree)
 {
 	queue_t q = {0, 0, NULL};
 	const binary_tree_t *tmp;
@@ -44,12 +78,6 @@ int binary_tree_is_heap(const binary_tree_t *tree)
 		}
 		else
 			is_not_full = 1;
-
-		if (tmp->left && tmp->left->n > tmp->n)
-			return (0);
-		if (tmp->right && tmp->right->n > tmp->n)
-			return (0);
-
 		/* dequeue */
 		tmp = q.queue[q.front++];
 	}
