13655f2acb337f6554e9f1bac2b6e8a03b0e63fa
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-02-06 00:56:50 +0300

    Build max heap from an array

--- 132-array_to_heap.c
+++ 132-array_to_heap.c
@@ -1,13 +1,22 @@
 #include "binary_trees.h"
 
 /**
- * array_to_heap - write your short description
+ * array_to_heap - builds a Max Binary Heap tree from an array
  * @array: pointer to 1st element in array
  * @size: number of elements in array
  *
- * Return: 0 as exit status
+ * Return: pointer to the root of Binary Heap, or NULL on failure
  */
 heap_t *array_to_heap(int *array, size_t size)
 {
-	return (0);
+	size_t i = 0;
+	heap_t *root = NULL;
+
+	if (!array || size == 0)
+		return (root);
+
+	for (i = 0; i < size; i++)
+		heap_insert(&root, array[i]);
+
+	return (root);
 }
